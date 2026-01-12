eb0ed1aae600f63fa783be643810577d2e9b14b0
Prasad Pawar <164699888+Prasadp95@users.noreply.github.com>
2026-01-12 20:40:56 +0530

    SQL Files: Functions
    SQL Files: Functions

---
+++ get_QTR.sql
@@ -0,0 +1,16 @@
+CREATE DEFINER=`root`@`localhost` FUNCTION `get_qtr`(
+calender_date date
+) RETURNS char(2) CHARSET utf8mb4
+    DETERMINISTIC
+BEGIN
+declare m int;
+declare qtr char(2);
+set m = month(calender_date);
+	case
+		when m in (9,10.11) then set qtr="Q1";
+        when m in (12,1,2) then set qtr="Q2";
+        when m in (3,4,5) then set qtr="Q3";
+        else set qtr="Q4";
+    end case;
+RETURN qtr;
+END
---
+++ get_fiscal_year.sql
@@ -0,0 +1,8 @@
+CREATE DEFINER=`root`@`localhost` FUNCTION `get_fiscal_year`(
+	calender_date date ) RETURNS int
+    DETERMINISTIC
+BEGIN
+declare fiscal_year int;
+set fiscal_year = year(date_add(calender_date, interval 4 month));
+RETURN fiscal_year;
+END
